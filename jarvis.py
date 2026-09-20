7e781e039bb6cc2e1de3a1fa59c867983133ef66
cartercreatedit <carterfr0708@icloud.com>
2026-09-20 19:47:25 +0800

    Fix syntax error in model parameter assignment

--- jarvis.py
+++ jarvis.py
@@ -182,7 +182,7 @@ Current time: {current_time}
 
     try:
         response = client.chat.completions.create(
-            model="gpt-4o-mini"
+            model="gpt-4o-mini",
             messages=messages,
             temperature=0.5,
             max_tokens=250
