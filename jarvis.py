46853fb5474fe01d29023c63ce307d6a9636ec5e
cartercreatedit <carterfr0708@icloud.com>
2026-09-21 17:31:51 +0800

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -508,8 +508,6 @@ st.components.v1.html(f"""
         }});
 
         requestAnimationFrame(renderDenseGrid);
-    }
-
     window.addEventListener('message', (e) => {{
         if (e.data && e.data.type === 'jarvis_audio_state') {{
             isJarvisSpeaking = e.data.speaking;
